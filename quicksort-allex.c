e107c41507c2ac20d50aea2609a8cc8a3e698883
Allex Lima <allexlima97@gmail.com>
2016-06-14 18:02:39 -0400

    Versão não completa

---
+++ quicksort-allex.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define _tam 10
+#define _posPiv
+
+void quicksort(int *vector, int begin, int end){
+	int pivo;
+	if(end > begin){
+		int pivo = oi;
+	}
+}
+
+int getPivo(int *vector, int begin, int end){
+	int pivo = ,
+		xA = vector[begin], //Extremidade esquerda do vetor
+		xB = vector[end]; //Extremidade direira do vetor
+		while()
+		
+}
+
+int main(int argc, char *argv[]){
+	return 0;
+}
